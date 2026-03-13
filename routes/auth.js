f890838f71a6bd85a30afa64be28a0bb2fade809
bhavik0223 <patelbhavik2314@gmail.com>
2026-03-14 01:25:22 +0530

    Fixed login authentication

--- routes/auth.js
+++ routes/auth.js
@@ -21,15 +21,15 @@ router.post("/signup", async (req, res) => {
       return res.status(400).json({ message: "Email already registered" });
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await User.create({
-      name,
-      email: lowerEmail,
-      password: hashedPassword,
-      role: "user"
-    });
+
+  const user = await User.create({
+  name,
+  email: lowerEmail,
+  password: password,
+  role: "user"
+});
+
 
     res.status(201).json({
       message: "Signup successful",
@@ -65,7 +65,7 @@ router.post("/login", async (req, res) => {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+const isMatch = await user.matchPassword(password);
 
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid email or password" });
