bbf38c166272d0024a7e5ee6ede1e5173de80023
bhavik0223 <patelbhavik2314@gmail.com>
2026-04-10 18:05:46 +0530

    fix

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const Issue = require("../models/Issue");
 const express = require("express");
 const router = express.Router();
 
@@ -13,6 +14,19 @@ router.get("/issues", (req, res) => {
   res.json({
     message: "Admin viewing all issues"
   });
+});
+
+
+router.get("/all", async (req,res)=>{
+const issues = await Issue.find().sort({createdAt:-1});
+res.json(issues);
+});
+
+router.put("/status/:id", async (req,res)=>{
+await Issue.findByIdAndUpdate(req.params.id,{
+status:req.body.status
+});
+res.json({message:"Updated"});
 });
 
 module.exports = router;
